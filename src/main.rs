6d2a62bb53b50b05af074a15ee0d2ea1e0735346
Bryant Deters <bryantdeters@gmail.com>
2018-08-05 20:07:19 -0500

    Replaced the unwrap calls because I'd like this proof of concept to run for more than an hour.

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,14 @@ use algorithmia::Algorithmia;
 
 fn main() {
     //Algorithmia stuff
-    let alg_token = env::var("ALGORITHMIA_TOKEN").unwrap();
+    let alg_token = env::var("ALGORITHMIA_TOKEN").expect("Missing ALGORITHMIA_TOKEN value");
     let client = Algorithmia::client(alg_token);
     let alg = client.algo("nlp/SocialSentimentAnalysis/0.1.4");
 
     //Telegram stuff
     let mut core = Core::new().unwrap();
-    let token = env::var("TELEGRAM_BOT_TOKEN").unwrap();
-    let api = Api::configure(token).build(core.handle()).unwrap();
+    let token = env::var("TELEGRAM_BOT_TOKEN").expect("Missing TELEGRAM_BOT_TOKEN value");
+    let api = Api::configure(token).build(core.handle()).expect("The API token may not be correct");
 
     // Fetch new updates via long poll method
     let future = api.stream().for_each(|update| {
@@ -30,17 +30,23 @@ fn main() {
         if let UpdateKind::Message(message) = update.kind {
             if let MessageKind::Text {ref data, ..} = message.kind {
                 println!("<{}>: {}", &message.from.first_name, data);
-                let resp = alg.pipe(data).unwrap();
-                let s: Vec<SentimentResponse> = serde_json::from_str(&resp.to_string()).unwrap();
-                api.spawn(message.text_reply(
-                    format!("compound:{}\nnegative:{}\nneutral:{}\npositive:{}", s[0].compound, s[0].negative, s[0].neutral, s[0].positive)
-                ));
+                if let Ok(resp) = alg.pipe(data) {
+                    match serde_json::from_str::<Vec<SentimentResponse>>(&resp.to_string()){
+                        Ok(s) => api.spawn(message.text_reply(
+                            format!("compound:{}\nnegative:{}\nneutral:{}\npositive:{}", s[0].compound, s[0].negative, s[0].neutral, s[0].positive)
+                        )),
+                        Err(e) => println!("{}", e),
+                    }
+                }
+
             }
         }
         Ok(())
     });
-
-    core.run(future).unwrap();
+    match core.run(future) {
+        Ok(f) => f,
+        Err(f) => println!("{}", f),
+    }
 }
 
 #[derive(Deserialize)]
