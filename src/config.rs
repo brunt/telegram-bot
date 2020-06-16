70d60c3afa14dd6cad734500a40cc0e07aae23f2
Bryant Deters <bryantdeters@gmail.com>
2020-06-15 19:24:03 -0500

    Added prometheus metrics + actix server

--- src/config.rs
+++ src/config.rs
@@ -8,6 +8,7 @@ pub(crate) struct Config {
     pub spending_total_url: String,
     pub spending_reset_url: String,
     pub forecast_token: String,
+    pub webserver_port: String,
 }
 
 impl Config {
@@ -20,6 +21,7 @@ impl Config {
             spending_reset_url: env::var("SPENDING_API_RESET")
                 .expect("Missing SPENDING_API_URL value"),
             forecast_token: env::var("FORECAST_TOKEN").expect("Missing FORECAST_TOKEN"),
+            webserver_port: env::var("BOT_METRICS_PORT").expect("Missing BOT_METRICS_PORT value"),
         })
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use actix_web::{App, HttpServer};
+use actix_web_prom::{PrometheusMetrics};
 use futures::StreamExt;
+use prometheus::{opts, IntCounterVec};
+use std::sync::Arc;
 use teloxide::prelude::*;
 
-use std::sync::Arc;
 
 mod arrival;
 mod config;
@@ -12,22 +15,34 @@ use config::Config;
 use spending::{help_spending, is_spent_category_request, is_spent_request, parse_spent_request};
 use weather::{help_weather, weather_request};
 
-#[tokio::main]
+#[actix_rt::main]
 async fn main() {
-    run().await; //recommended pattern
+    let prometheus = PrometheusMetrics::new("teloxide", Some("/metrics"), None);
+    let counter_opts = opts!("counter", "forecast").namespace("teloxide");
+    let counter = IntCounterVec::new(counter_opts, &["request"]).unwrap();
+    prometheus
+        .registry
+        .register(Box::new(counter.clone())).unwrap();
+    let config = Config::from_env();
+    HttpServer::new(move || App::new().wrap(prometheus.clone()))
+        .bind(format!("0.0.0.0:{}", &config.webserver_port))
+        .expect("address in use")
+        .run();
+    run(config, counter).await;
 }
 
-async fn run() {
+async fn run(config: Arc<Config>, counter: IntCounterVec) {
     let bot = Bot::from_env();
-    let config = Config::from_env();
     Dispatcher::new(bot)
         .messages_handler(move |rx: DispatcherHandlerRx<Message>| {
             rx.for_each_concurrent(None, move |msg| {
                 let config = Arc::clone(&config);
+                let counter = counter.clone();
                 async move {
                     match msg.update.text() {
                         None => {
                             if let Some(loc) = msg.update.location() {
+                                counter.with_label_values(&["Weather"]).inc();
                                 msg.answer(
                                     weather_request(
                                         &config.forecast_token,
@@ -43,18 +58,23 @@ async fn run() {
                         }
                         Some(txt) => match txt {
                             x if x.eq("Help") => {
+                                counter.with_label_values(&["Help"]).inc();
                                 msg.answer(helpmsg()).send().await.unwrap();
                             }
                             x if x.eq("Help schedule") => {
+                                counter.with_label_values(&["Help schedule"]).inc();
                                 msg.answer(help_schedule()).send().await.unwrap();
                             }
                             x if x.eq("Help spending") => {
+                                counter.with_label_values(&["Help spending"]).inc();
                                 msg.answer(help_spending()).send().await.unwrap();
                             }
                             x if x.eq("Help weather") => {
+                                counter.with_label_values(&["Help weather"]).inc();
                                 msg.answer(help_weather()).send().await.unwrap();
                             }
                             x if is_next_arrival_request(x) => {
+                                counter.with_label_values(&["Next Arrival"]).inc();
                                 let data_vec: Vec<&str> = x.splitn(2, ' ').collect();
                                 match next_arrival_request(
                                     &config.metro_api_url,
@@ -77,6 +97,7 @@ async fn run() {
                                 }
                             }
                             x if is_spent_request(x) => {
+                                counter.with_label_values(&["Spending"]).inc();
                                 let split: Vec<&str> = x.split(' ').collect();
                                 if is_spent_category_request(x) {
                                     msg.answer(
